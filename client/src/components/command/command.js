24d4df258b6502cec6dd258d5daa1c1ce5d55d83
root <root@AbhijeetSingh>
2023-09-19 23:41:57 +0530

    command list is comming

--- client/src/components/command/command.js
+++ client/src/components/command/command.js
@@ -1,10 +1,13 @@
 import React, { useState, useEffect } from "react";
-import { Button } from "react-bootstrap";
+import { Button,Card,ListGroup } from "react-bootstrap";
+import "./command.css"
+import {useHistory} from "react-router-dom"
 
 const CommandPage = (props) => {
   const { serverName } = props.match.params;
   const [serverData, setServerData] = useState([]); // Initialize as an empty array
   const [serverDetails, setServerDetails] = useState({});
+  const history = useHistory()
 
   useEffect(() => {
     const apiUrl = `http://localhost:8001/command/server/${serverName}`;
@@ -16,12 +19,16 @@ const CommandPage = (props) => {
     })
       .then((response) => response.json())
       .then((data) => {
-        setServerData( Object.values(data)); // Now serverData is an array
+        setServerData(data.data); // Now serverData is an array
       })
       .catch((error) => {
-        console.error('Error:', error);
+        console.error("Error:", error);
+        // Show an alert message
+        alert("An error occurred. Redirecting to home page.");
+        // Redirect to the home page
+        history.push("/");
       });
-  }, [serverName]);
+  }, [serverName,history]);
 
   useEffect(() => {
     const apiUrl = `http://localhost:8001/servers/${serverName}`;
@@ -33,21 +40,62 @@ const CommandPage = (props) => {
     })
       .then((response) => response.json())
       .then((data) => {
-        setServerDetails(data);
+        setServerDetails(data[0]);
       })
       .catch((error) => {
-        console.error('Error:', error);
+        console.error("Error:", error);
+        // Show an alert message
+        alert("An error occurred. Redirecting to home page.");
+        // Redirect to the home page
+        history.push("/");
       });
-  }, [serverName]);
+  }, [serverName,history]);
 
-  console.log(typeof(serverData))
-  console.log(serverData)
+//   console.log(typeof(serverData))
+  const arrayData = []
+  for(let i = 0 ; i < serverData.length ; i++){
+    arrayData.push(serverData[i])
+  }
 
   return (
     <div>
-      <h1>
-        <Button>Run command on the server</Button>
-      </h1>
+        <h1>
+          <Button>Run command on the server</Button>
+        </h1>
+
+        <span/>
+        <div className="serverDetails">
+        <Card style={{ width: '18rem' }}>
+            <Card.Header>Name: {serverDetails.serverName}</Card.Header>
+            <ListGroup variant="flush">
+                <ListGroup.Item>Host: {serverDetails.host}</ListGroup.Item>
+                <ListGroup.Item>User: {serverDetails.userName}</ListGroup.Item>
+                <ListGroup.Item>ID: {serverDetails._id}</ListGroup.Item>
+             </ListGroup>
+        </Card>
+        </div>
+        <span/>
+
+        {arrayData.map((item,index)=>(
+            <div key={index}>
+            <Card>
+            <Card.Header>Here date and time will come</Card.Header>
+            <Card.Body>
+              <blockquote className="blockquote mb-0">
+                <pre>
+                  {' '}
+                  {item["outPut"]}
+                  {' '}
+                </pre>
+                <footer className="blockquote-footer">
+                  command run is <cite title="Source Title">{item["command"]}</cite>
+                </footer>
+              </blockquote>
+            </Card.Body>
+          </Card>
+            </div>
+        ))}
+       
     </div>
   );
 };
